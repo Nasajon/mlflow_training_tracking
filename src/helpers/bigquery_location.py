33f61f5fe9041f12d0ef591b4f92543b45d66ca9
Nilton Duarte <nilton.gduarte@gmail.com>
2020-10-14 19:11:33 -0300

    bigquery_location class - added optional target_column to deal with 'y' and others datasets properly

--- src/helpers/bigquery_location.py
+++ src/helpers/bigquery_location.py
@@ -3,17 +3,23 @@ class BigQueryLocation:
     base_select_query = """
     SELECT {columns} FROM `{table}` ORDER BY {order}"""
 
-    def __init__(self, columns, id_column, table, order, limit=None):
-        self.columns = columns
+    def __init__(self, data_columns, id_column, table, order, target_column=None, limit=None):
+        data_columns = data_columns.copy()
+        self.data_columns = data_columns
+        self.target_column = target_column
         self.id_column = id_column
         self.table = table
         self.order = order
         self.limit = limit
 
     def get_select_query(self, include_id=False):
+        columns = self.data_columns.copy()
+        if self.target_column:
+            columns.append(self.target_column)
+        if include_id:
+            columns.append(self.id_column)
         select_query = self.base_select_query.format(
-            columns=', '.join([self.id_column, *self.columns] if include_id
-                              else self.columns),
+            columns=', '.join(columns),
             table=self.table,
             order=self.order)
         if self.limit:
@@ -22,10 +28,11 @@ class BigQueryLocation:
 
     def __repr__(self):
         repr = {
-            'columns': self.columns,
+            'data_columns': self.data_columns,
             'id_column': self.id_column,
             'table': self.table,
             'order': self.order,
+            'target_column': self.target_column,
             'limit': self.limit
         }
         return str(repr)
