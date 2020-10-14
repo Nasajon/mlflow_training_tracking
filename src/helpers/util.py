1d54828e78c0b1ea818a25af05190b68e31aaf4d
Nilton Duarte <nilton.gduarte@gmail.com>
2020-10-14 10:27:13 -0300

    helper.util - added decorator to permanently and temporary remove column

--- src/helpers/util.py
+++ src/helpers/util.py
@@ -1,5 +1,6 @@
 
 import functools
+import pandas as pd
 
 
 def force_async(fn):
@@ -15,4 +16,42 @@ def force_async(fn):
         partial_func = functools.partial(fn, *args, **kwargs)
         return await loop.run_in_executor(None, partial_func)
 
+    return wrapper
+
+
+def df_temporary_remove_column(df_variable: str, column_property_name: str):
+    """
+    Decorator to temporary remove column from data frame and add it later
+    """
+
+    @functools.wraps(df_variable, column_property_name)
+    def wrapper(fn):
+
+        @functools.wraps(fn)
+        def wrapper_fn(self, *args, **kwargs):
+            column = getattr(self, column_property_name)
+            df = kwargs[df_variable].copy()
+            df_column = df.pop(column).to_frame()
+            kwargs[df_variable] = df
+            ret_df = fn(self, *args, **kwargs)
+            ret = df_column.join(ret_df)
+            return ret
+        return wrapper_fn
+    return wrapper
+
+
+def df_permanently_remove_column(df_variable: str, column_property_name: str):
+    """
+    Decorator to permanently remove column from data frame
+    """
+
+    @functools.wraps(df_variable, column_property_name)
+    def wrapper(fn):
+        @functools.wraps(fn)
+        def wrapper_fn(self, *args, **kwargs):
+            column = getattr(self, column_property_name)
+            kwargs[df_variable] = kwargs[df_variable].drop(
+                columns=column, inplace=False)
+            return fn(self, *args, **kwargs)
+        return wrapper_fn
     return wrapper
