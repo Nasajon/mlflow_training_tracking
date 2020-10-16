5f8d64a9f8ce0c5c64341247850e29d0e5f60914
Nilton Duarte <nilton.gduarte@gmail.com>
2020-10-16 17:12:54 -0300

    metrics.numpy_array - added evaluation binary class metric class

--- src/service_implementations/evaluation_metrics/numpy_array.py
+++ src/service_implementations/evaluation_metrics/numpy_array.py
@@ -1,6 +1,8 @@
 from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error, median_absolute_error, r2_score, max_error
+from sklearn.metrics import confusion_matrix
 from sklearn.metrics._regression import _check_reg_targets
 from service_interfaces.evaluation_regression_metrics_interface import EvaluationRegressionsMetricsOperatorInterface
+from service_interfaces.evaluation_classification_metrics_interface import EvaluationBinaryClassificationMetricsOperatorInterface
 import numpy as np
 from helpers.util import df_permanently_remove_column
 
@@ -49,3 +51,61 @@ class EvaluationRegressionMetricsNumpyArray(EvaluationRegressionsMetricsOperator
                     "metrics": {"percentile_absolute_error": perc[index]}, 'step': index
                 })
         return metric_list
+
+
+class EvaluationBinaryClassificationMetricsNumpyArray(EvaluationBinaryClassificationMetricsOperatorInterface):
+
+    def __init__(self, probability_threshold):
+        self.probability_threshold = probability_threshold
+
+    @df_permanently_remove_column(df_variable='y_true', column_property_name='row_id_column')
+    @df_permanently_remove_column(df_variable='y_pred', column_property_name='row_id_column')
+    def load_data(self, y_true, y_pred, *args, **kwargs):
+        y_type, y_true, y_pred, multioutput = _check_reg_targets(
+            y_true, y_pred, multioutput='uniform_average')
+        self.y_true = y_true
+        self.y_pred = y_pred
+        self.matrix_loaded = False
+
+    @property
+    def confusion_matrix(self):
+        if not self.matrix_loaded:
+            self.confusion_matrix = confusion_matrix(self.y_true, self.y_pred).ravel()
+            self.matrix_loaded = True
+        return self.confusion_matrix
+
+    def true_negative(self):
+        return confusion_matrix[0]
+
+    def false_positive(self):
+        return confusion_matrix[1]
+
+    def false_negative(self):
+        return confusion_matrix[2]
+
+    def true_positive(self):
+        return confusion_matrix[3]
+
+    def precision(self):
+        """
+        The precision is the ratio tp / (tp + fp) where tp is the number of true positives
+        and fp the number of false positives.
+        The precision is intuitively the ability of the classifier not to label 
+        as positive a sample that is negative.
+        """
+        return self.true_positive()/(self.true_positive() + self.false_positive())
+
+    def true_positive_rate(self):
+        """
+        The recall (true_positive_rate) is the ratio tp / (tp + fn) where tp is the number
+        of true positives and fn the number of false negatives.
+        The recall is intuitively the ability of the classifier to find all the positive samples.
+        """
+        return self.true_positive()/(self.true_positive() + self.false_negative())
+
+    def true_negative_rate(self):
+        """
+        Specificity (true_negative_rate) measures the proportion of negatives that are correctly identified
+        tn / (tn + fp)
+        """
+        return self.true_negative()/(self.true_negative() + self.false_positive())
