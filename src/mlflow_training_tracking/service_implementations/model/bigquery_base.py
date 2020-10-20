a0f62c5d975735d01ba01be01d2a66a2430437e2
Nilton Duarte <nilton.gduarte@gmail.com>
2020-10-20 19:51:55 -0300

    moved get_train_metrics to bigquery_base as its the same for the majority of the models

--- src/mlflow_training_tracking/service_implementations/model/bigquery_base.py
+++ src/mlflow_training_tracking/service_implementations/model/bigquery_base.py
@@ -103,3 +103,49 @@ SELECT * FROM ML.FEATURE_IMPORTANCE (MODEL `{sql_model_path}`)
     def load(self, folder_path: str, *args, **kwargs) -> None:
         pass
 
+    def get_train_metrics(self, *args, **kwargs) -> 'list[dict]':
+        """Return a list of dictionaries to log in metrics MLFlow server
+
+        Returns:
+            list[dict]: E.g.
+            [
+                {
+                metrics: {
+                    validadtion_1.metric_1: value,
+                    validadtion_1.metric_2: value,
+                    validadtion_2.metric_1: value,
+                    validadtion_2.metric_2: value
+                    },
+                step: 1
+                },
+                                {
+                metrics: {
+                    validadtion_1.metric_1: value,
+                    validadtion_1.metric_2: value,
+                    validadtion_2.metric_1: value,
+                    validadtion_2.metric_2: value
+                    },
+                step: 2
+                }
+            ]
+        """
+        metrics_struct_list = []
+        train_metric_query = self.train_metric_query.format(
+            sql_model_path=self.sql_model_path)
+        query_job = self.run_query_and_wait(train_metric_query,
+                                            job_id_prefix=self.job_id_prefix)
+        rows = query_job.result()
+
+        for row in rows:
+            row_metric = {
+                'loss': row.loss,
+                'eval_loss': row.eval_loss,
+                'duration_ms': row.duration_ms,
+                'learning_rate': row.learning_rate
+            }
+            metrics_struct_list.append({
+                'metrics': row_metric,
+                'step': row.iteration
+            })
+
+        return metrics_struct_list
--- src/mlflow_training_tracking/service_implementations/model/bigquery_classification.py
+++ src/mlflow_training_tracking/service_implementations/model/bigquery_classification.py
@@ -6,33 +6,4 @@ from mlflow_training_tracking.service_implementations.model.bigquery_base import
 
 
 class ClassificationModelOperatorBigQueryLocation(ModelOperatorBigQueryLocation):
-
-    def get_train_metrics(self, *args, **kwargs) -> 'list[dict]':
-        """Return a list of dictionaries to log in metrics MLFlow server
-
-        Returns:
-            list[dict]: E.g.
-            [
-                {
-                metrics: {
-                    validadtion_1.metric_1: value,
-                    validadtion_1.metric_2: value,
-                    validadtion_2.metric_1: value,
-                    validadtion_2.metric_2: value
-                    },
-                step: 1
-                },
-                                {
-                metrics: {
-                    validadtion_1.metric_1: value,
-                    validadtion_1.metric_2: value,
-                    validadtion_2.metric_1: value,
-                    validadtion_2.metric_2: value
-                    },
-                step: 2
-                }
-            ]
-        """
-        metrics_struct_list = []
-
-        return metrics_struct_list
+    pass
